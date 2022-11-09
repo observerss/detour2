e10619673a2ac04f9ff5bb0bbf0ff01dd4022628
observerss <jingchaohu@gmail.com>
2022-11-09 11:40:35 +0800

    chore: improve build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-set -ex
 if [ "$1" == "" ]; then
     echo "usage: ./buish.sh VERSION"
     exit
 fi
+
+set -ex
 VERSION="$1"
+
+echo "docker build"
 docker build -t detour2 .
 docker save -o dist/detour2.tar detour2
 rm -f dist/detour2.tar.gz; gzip dist/detour2.tar
+
+echo "docker push"
 docker tag detour2:latest registry.cn-hongkong.aliyuncs.com/hjcrocks/detour2:$VERSION
 docker push registry.cn-hongkong.aliyuncs.com/hjcrocks/detour2:$VERSION
+
+echo "git tag and push"
+git tag v"$1" -f
+git push --tag -f
